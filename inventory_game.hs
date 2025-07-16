a7b482d54c31d84b5da62ed08620e83c2301d327
Anna Gan <annaxjgan@gmail.com>
2025-07-16 12:59:08 +1000

    added inventory game

---
+++ inventory_game.hs
@@ -0,0 +1,39 @@
+import Control.Monad.State
+
+
+-- Test-based inventory tracker, each item is represented as a string (e.g. "potion", "sword")
+-- inventory is a list of items
+
+-- Each pickup item adds item to the inventory
+-- Each use item removes the item if it exists, otherwise, the inventory is unchanged
+
+type Inventory = [String]
+data Action = PickUp String | Use String
+
+-- This adds the item to the beginning of the inventory
+pickup :: String -> State Inventory ()
+pickup x = do
+    currState <- get 
+    put (x:currState)
+    return ()
+
+-- This removes one copy of the item from the inventory if present
+-- Hint: use get, filter and put
+use :: String -> State Inventory ()
+use x = do
+    n <- get 
+    put (filter (/= x) n)
+    return ()
+
+-- takes a list of actions and runs them in order
+performActions :: [Action] -> State Inventory ()
+performActions [] = return ()
+performActions (action:actions) = do
+    case action of 
+        PickUp x -> pickup x
+        Use x -> use x
+    performActions actions
+
+-- runs the game and start with an empty inventory and returns the final inventory after performing all the actions
+runGame :: [Action] -> Inventory
+runGame actions = execState (performActions actions) []
